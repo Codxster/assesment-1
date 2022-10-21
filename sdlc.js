c74a976114abfe718dab236cfdf64b4049692272
Cody Barlow <barlow.cody8@gmail.com>
2022-10-21 11:29:58 -0600

    sdlc finished

--- sdlc.js
+++ sdlc.js
@@ -5,12 +5,20 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
-// Planning
+//
+//
+//
+//
+//
+//
+//
+
+// Implementation ---
+// Design ---
+// Maintain ---
+// Analysis of Requirements 
+// Testing & Integration ---
+// Planning ---
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +26,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const design = "This is the second step, this is where you put the pen to the paper, this is typically where the prototype is created"
+const implementation = "This is the third step, after you're done designing, you can now start implementing the foundational coding to your project so it comes to life "
+const testing = "This is the fourth step, this is where you find out of your prototype is functioning properly and does what it's meant to do"
+const analysisRequirements = "This is the fifth step, this is where you set your standards, how do you want to run your product? this is where your customer needs meets the businesses expectations"
+const maintain = "This is the last step, probably the most important. After you're done with your masterpiece, now you have to take care of it. Make sure things continue to run properly."
